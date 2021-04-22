15c2542c8a1f0c743ffe925758caf67372fb98d5
Gino Su <syjgino@gmail.com>
2021-04-22 15:53:46 -0700

    Update IntensityViewer.py

--- IntensityViewer.py
+++ IntensityViewer.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Nov 18 15:05:52 2020
+This file is part of the Shotgun Lipidomics Assistant (SLA) project.
 
-@author: BaolongSu
+Copyright 2020 Baolong Su (UCLA), Kevin Williams (UCLA)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
+
+
 import numpy as np
 import pandas as pd
 from pyopenms import *
